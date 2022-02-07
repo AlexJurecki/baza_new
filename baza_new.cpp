9e2ae51c8c75a6c15adce62597aa5e1fe4a3aaa7
AlexJurecki <aljurecki@gmail.com>
2022-02-08 00:56:04 +0100

    added comms

--- baza_new.cpp
+++ baza_new.cpp
@@ -1,7 +1,4 @@
-﻿// baza.cpp: definiuje punkt wejścia dla aplikacji.
-//
-
-#include "baza_new.h"
+﻿#include "baza_new.h"
 #include "pojazdy.hpp"
 #include "randomgen.h"
 #include <vector>
@@ -9,15 +6,19 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
-#include <memory>
+//#include <memory>
 #include <cstring>
 
 
 class Data {
 public:
-	Data() { cout << "Welcome to the Data!" << endl; }
-	void dodaj_random(char  type) {
-		switch (type) {
+
+	Data() { cout << "Welcome to the Data!" << endl; }//domyslny
+
+
+
+	void dodaj_random(char  type) { //funkcja dodaje pojazd o randomwych param
+		switch (type) { // rodzaj zalezy od typu
 		case 'M':
 			Katalog.push_back(make_shared<Motocykl>());
 			
@@ -30,56 +31,56 @@ public:
 			break;
 
 		}
-		Katalog.back()->setTyp(type);
-		Katalog.back()->setId(numer);
+		Katalog.back()->setTyp(type);//kwestia ulatiwajaca wizualizacje i pozniejszy odczyt z pliku
+		Katalog.back()->setId(numer);//numerator ID
 		numer += 1;
 	}
 
+
+
+
 	template<typename tname>
-	void getcin(tname& arg) {
+	void getcin(tname& arg) {//dosc prosty template, wykonuje cin dla roznych typow
 			cin >> arg;
 				}
 
+
+
 	void dodaj(char type)
 	{
-		//string	a="BMW";
-		//string b="K1200";
-		//int c=2010;
-		//double d=110;
-		//double e=35.5;
-		//bool	f=true;
-		// 
-		//Vehicles.push_back(Motocykl(a, b, c, d, e, f));
+
 		cout << "Would you like to randomize? yes/no" << endl;
 		string question;
-		cin >> question;
-		if ((question == "yes") || (question == "y")) {
+		//cin >> question;
+		getcin(question);
+		if ((question == "yes") || (question == "y")) { //randomizownie param jest mozliwe rowniez na tym etapie
 			switch (type) {
-			case 'M': Katalog.push_back(make_shared<Motocykl>());
+			case 'M': Katalog.push_back(make_shared<Motocykl>()); //pushback - dodwanie klasy dziedziczacej po poejdzie jako ostatni element wektora
 				break;
-				
+
 			case 'O': Katalog.push_back(make_shared<Osobowy>());
 				break;
-					
+
 			case 'L': Katalog.push_back(make_shared<LKW>());
 				break;
 			}
 		}
-		else {
+		else { //customow wpisywane dane do parametrow pojazdow
 			string a, b;
 			int c;
 			double d, e;
-			
+
 			cout << endl << "Podaj marke:\t";
 			cin >> a;
 			cout << "Podaj model:\t";
 			cin >> b;
 			cout << endl << "Podaj Date produkcji:\t";
 			cin >> c;
-			try {
+			try { //exception - rok nie moze byc zbyt maly, wiemy kiedy powstal pierwszy automobile
 				if (c < 1886) {
-					throw c;}
+					throw c;
 				}
+			}
 			catch (int ex) {
 				cout << "Exception - the first car  " << endl << " the Benz Patent-Motorwagen was built in 1886" << endl;
 				c = 1886;
@@ -89,19 +90,19 @@ public:
 			cout << endl << "Podaj pojemnosc:\t";
 
 			cin >> d;
-			try {
+			try { //exception -  pojemnosc nie moze byc niedodatnia
 				if (d <= 0) {
 					throw d;
 				}
 			}
 			catch (double ex) {
-				cout << "Exception - pojemnosc is not  " <<d << " you're wrong!" << endl;
+				cout << "Exception - pojemnosc is not  " << d << " you're wrong!" << endl;
 				d = 100;
 			}
 			cout << endl << "Podaj moc:\t";
 
 			cin >> e;
-			try {
+			try {//exception-  moc nie moze byc niedodatnia
 				if (e <= 0) {
 					throw e;
 				}
@@ -110,59 +111,52 @@ public:
 				cout << "Exception - power is not  " << e << " you're wrong!" << endl;
 				e = 50;
 			}
-			switch (type) {
+			switch (type) { //czesc parametrow nalezy do dziedziczacych klas, ponizej wpisywane sa ich param zaleznie od zadanego typu
 			case 'M':
-				cout << endl << "Podaj bool Boxer:\t";
+				cout << endl << "Podaj bool Boxer:\t"; //motocykl ma atrybut bool
 				bool f;
 				getcin(f);
-				//f=<int>getcin();
-			//	cin >> f;
+				//	cin >> f;
 				Katalog.push_back(make_shared<Motocykl>(a, b, c, d, e, f));
-				
+
 				break;
 			case 'O':
-				cout << endl << "Podaj int pasazerowie:\t";
+				cout << endl << "Podaj int pasazerowie:\t";//osobowy ma atrybut int
 				int ff;
 				getcin(ff);
 				Katalog.push_back(make_shared<Osobowy>(a, b, c, d, e, ff));
-				
+
 				break;
 			case 'L':
-				cout << endl << "Podaj double ladunek:\t";
+				cout << endl << "Podaj double ladunek:\t";//LKW ma atrybut  double
 				double fff;
 				getcin(fff);
 				Katalog.push_back(make_shared<LKW>(a, b, c, d, e, fff));
-				
+
 				break;
 
 			}
-			
 
-			//Katalog.push_back(make_shared<Motocykl>(a, b, c, d, e, f));
 		}
-		Katalog.back()->setTyp(type);
-		Katalog.back()->setId(numer);
+		Katalog.back()->setTyp(type); //kwestia ulatiwajaca wizualizacje i pozniejszy odczyt z pliku
+		Katalog.back()->setId(numer);//numerator dla nadawnia ID z sensem
 		numer += 1;
-		//Vehicles.push_back(make_unique<Motocykl>(a, b, c, d, e, f));
-		//Vehicles.push_back(Motocykl(a, b, c, d, e, f));
-		// 
-		//dum(new Motocykl(a, b, c, d, e, f));
 	}
 
-	void timeflow() {
-		//for (int i = 0; i < size(Katalog);++i) {
 
-		//	Katalog[i]++;
-		//}
-		//Pojazd&++(Katalog[0]);
+
+	void timeflow() { //funkcja wplywajaca na atrybut wiek - zwieksza go o jeden
+
 		for (auto& it : Katalog) {
 
 			++(*it);
 			it->print();
 		}
 	}
+
+
 	/*
-	template <typename T>
+	template <typename T> //to sie akurat nie udalo
 	//void maxi(const T (*func)()){
 	T maxi(const string& str){
 
@@ -176,44 +170,31 @@ public:
 		return
 	}*/
 
+
+
 	template <typename T>
-	T more(T x) {
+	T more(T x) { //template pozwala na porownanie mocy lub daty, zalezne od wyboru zastosujemy int lub double
 		
 		auto it = count_if(
 				Katalog.begin(),
 				Katalog.end(),
 				[x](shared_ptr<Pojazd>& arg) {if (is_same<T, double>::value) return (arg->getPower() > x); else if (is_same<T, int>::value) return (arg->getData()>x); });
-				return it;
+				return it; //zliczamy liczbe pojazdow, ktore posiadaja parametr wiekszy od zadanej wartosci
 			
 	}
 
 
 
-
-	void dodaj_cechy() {
-
-		cout << "Gimme cecha" << endl;
-		Katalog.back()->setCechy();
-	}
-
-	const void drukuj() {
+	const void drukuj() {//funkcja drukujaca
 		for (int i = 0; i < size(Katalog); ++i) {
-			//Vehicles[i].print();
 			cout << Katalog[i]->getId();
 			cout <<" ( "<< Katalog[i]->getTyp()<<" )"<< "\t";
-			Katalog[i]->print();
-			//	cout << "\tCechy:";
-				//list<string>::iterator it = Katalog[i]->getCechy().begin();
-				//for (it; it != Katalog[i]->getCechy().end(); *it++)
-				//	cout << *it <<", "<< endl;
+			Katalog[i]->print(); //korzystamy z metody print wewnatrz klas
 
-
-				//dynamic_pointer_cast<Motocykl>(Katalog[i])->print();
-				//dynamic_pointer_cast<Pojazd>(Katalog[i])->print();
-			switch (Katalog[i]->getTyp()) {
+			switch (Katalog[i]->getTyp()) { //zaleznie od getTyp - motocykl/LKW/osobowy
 			case 'M':
 
-				static_pointer_cast<Motocykl>(Katalog[i])->print();
+				static_pointer_cast<Motocykl>(Katalog[i])->print(); //static_ptr_cast do dostania sie do atrybutow nienalezacych bezposrednio do pojazdu
 				break;
 			case 'O':
 
@@ -225,35 +206,26 @@ public:
 				break;
 
 			}
-
-
-			//static_pointer_cast<Motocykl>(Katalog[i])->print();
-			//static_pointer_cast<Pojazd>(Katalog[i])->print();
-			//static_cast<Pojazd>(Vehicles[i]).print();
-			//cout << Katalog[i].getBool()<<
-			//stat_cast_ptr<Motocykl>(Vehicles[i]);
-			//auto derived=stat_cast_ptr<Motocykl>(Vehicles[i]);
 		}
 	}
 
-	const void sortuj() {
-		sort(Katalog.begin(), Katalog.end());
 
+
+	const void sortuj() {
+		//sort(Katalog.begin(), Katalog.end());
+		sort(Katalog.begin(), Katalog.end());
 	}
+
+
 
 	void sortby() {
 		cout << "m - moc, i - id, d- data, w-wiek" << endl;
 		string input;
 		cin >> input;
-		sort(Katalog.begin(), Katalog.end(), [input](shared_ptr<Pojazd>& arg1, shared_ptr<Pojazd>& arg2) {
-			//////return (arg1->data < arg2->data);
-			// 
-			//std::cout << arg.isMonster() << std::endl;
-			//if (arg.isMonster() == true) {
-
-				//return arg1->data>arg2->data; //bullshit afterawrds
+		sort(Katalog.begin(), Katalog.end(), [input](shared_ptr<Pojazd>& arg1, shared_ptr<Pojazd>& arg2) {//sortujemy po wybranej wartosci
 
 
+			//dosc skomplikowane lambda, zwracajaca sort wymaganych wartosci przez odczyt z konsoli
 			if (input == "m" || input == "moc") {
 				return (arg1->getPower() < arg2->getPower());
 			}
@@ -268,51 +240,36 @@ public:
 			else {
 				return (arg1->getId() < arg2->getId());
 			}
-			//}
-		//	else {
-			//	return 'y';
-		//	}/**/
-			});
-		//
 
+			});
 	}
+
+
 
 	void eraser(const int& wanted_id) {
-		//int i = 0;
-
-		//int wanted_id = 1;
-		//vector  <shared_ptr<Pojazd>>::iterator it = find(Katalog.begin(), Katalog.end(), wanted_id);
-		//auto it = find(Katalog.begin(), Katalog.end(), [](shared_ptr<Pojazd>& arg) {});
-		//
-	//	if (it != end(Katalog)) {
-		//	cout << "Element o id=" << wanted_id << " zajmuje pozycje "<< distance(Katalog.begin(), it) << endl;
-		//	Katalog.erase(it);		}
-	//	else {	cout << "Element is not present in the given baza danych";	}
 
 		auto it = find_if(Katalog.begin(), Katalog.end(), [wanted_id](const shared_ptr<Pojazd>& arg) {return arg->getId() == wanted_id; });
-		//auto b=in_range(Katalog.begin(), Katalog.end(), [wanted_id](const shared_ptr<Pojazd>& arg) {return arg->getId()==; });
-		//cout << *it;
-		
+
+		//usuwanie danego pojazdu z listy, wzywane sa dwa destruktory
 		
 		Katalog.erase(it);
-
 	}
 
-	void Write() {
-		ofstream File("baza_binar.bin", ios::out | ios::binary, ofstream::trunc);
+
+
+	void Write() { //zapis do pliku
+		ofstream File("baza_binar.bin", ios::out | ios::binary, ofstream::trunc); //otwieranie pliku, usuwnanie z niego poprzednich danych
 		if (File.is_open())
 		{
-			int size_of_database = size(Katalog);
+			int size_of_database = size(Katalog); //rozmiar listy w VS
 			File.write((char*)&size_of_database, sizeof(size_of_database));
 			cout << size_of_database << endl;
-			for (int i = 0; i < size(Katalog); ++i) {
-				//Vehicles[i].print();
-				//cout << Katalog[i]->getId() << "\t";
-				//Katalog[i]->print();
+			for (int i = 0; i < size(Katalog); ++i) { //petla po liczbie pojazdow
+	
 				int id = Katalog[i]->getId();
-				File.write((char*)&id, sizeof(id));
+				File.write((char*)&id, sizeof(id)); //zapis id (de facto juz zbedne)
 
-				size_t len = Katalog[i]->getMarka().size();
+				size_t len = Katalog[i]->getMarka().size(); //zapis stringow
 				File.write((char*)&len, sizeof(len));
 				File.write((char*)&Katalog[i]->getMarka()[0], len);
 
@@ -320,62 +277,57 @@ public:
 				File.write((char*)&len, sizeof(len));
 				File.write((char*)&Katalog[i]->getModel()[0], len);
 
-				int dat = Katalog[i]->getData();
+				int dat = Katalog[i]->getData(); //zapis int/double
 				double poj = Katalog[i]->getPojemnosc();
 				double pow = Katalog[i]->getPower();
 
-				//File.write((char*)&dat, sizeof(dat));
-				//	File.write((char*)&poj, sizeof(poj));
-				//	File.write((char*)&pow, sizeof(pow));
+
 				File.write((char*)&dat, sizeof(dat));
 				File.write((char*)&poj, sizeof(poj));
 				File.write((char*)&pow, sizeof(pow));
 
 
-				//dynamic_pointer_cast<Pojazd>(Katalog[i])->print();
-				if ((Katalog[i])->getTyp() == 'M') {
+				if ((Katalog[i])->getTyp() == 'M') {//zaleznie od Typu, zapis odpowiedniej wartosci : bool/int/double
 					char typ = 'M';
 					File.write((char*)&typ, sizeof(typ));
 					bool box = static_pointer_cast<Motocykl>(Katalog[i])->getBool();
 					File.write((char*)&box, sizeof(box));
 				}
-				else if ((Katalog[i])->getTyp()== 'O') {
+				else if ((Katalog[i])->getTyp()== 'O') {//zaleznie od Typu, zapis odpowiedniej wartosci : bool/int/double
 					char typ = 'O';
 					File.write((char*)&typ, sizeof(typ));
 					int pas = static_pointer_cast<Osobowy>(Katalog[i])->getPasazerowie();
 					File.write((char*)&pas, sizeof(pas));
 				}
-				else if ((Katalog[i])->getTyp() == 'L') {
+				else if ((Katalog[i])->getTyp() == 'L') {//zaleznie od Typu, zapis odpowiedniej wartosci : bool/int/double
 					char typ = 'L';
 					File.write((char*)&typ, sizeof(typ));
 					double lad = static_pointer_cast<LKW>(Katalog[i])->getLadunek();
 					File.write((char*)&lad, sizeof(lad));
 				}
-
-
 			}
-			// No need. The file will be closed when the function returns.
-			// binFile.close();
 		}
 	}
 
 
-	void Read() {
 
-		ifstream File("baza_binar.bin", ios::in | ios::binary);
+
+	void Read() {//odczyt z pliku binarnego
+
+		ifstream File("baza_binar.bin", ios::in | ios::binary); //otwieramy plik
 		if (File.is_open())
 		{
-			int size_of_database;
+			int size_of_database; //zczytujemy rozmiar databse w pliku
 			File.read((char*)&size_of_database, sizeof(size_of_database));
 			cout << endl << "Size of databse " << size_of_database<<endl;
-			for (int i = 0; i < size_of_database; i++) {
+			for (int i = 0; i < size_of_database; i++) { //petla - zczytywawnie wszystkich pojazdow
 
 				int id, da; double pow, poj; char typ; string mod, mar;
 
 				File.read((char*)&id, sizeof(id));
 
 				size_t len = 0;
-				File.read((char*)&len, sizeof(len));
+				File.read((char*)&len, sizeof(len)); //w ten sposob zczytujemy string
 				mar.resize(len);
 				File.read((char*)&mar[0], len);
 				//cout << mar;
@@ -389,135 +341,96 @@ public:
 				File.read((char*)&poj, sizeof(poj));
 				File.read((char*)&pow, sizeof(pow));
 
-				File.read((char*)&typ, sizeof(typ));
+				File.read((char*)&typ, sizeof(typ)); //determinacja jakiego typu jest pojazd w pliku
 				if (typ == 'M') {
-					bool box;
+					bool box; //zaleznie od typu -> rozne atrybuty (bool, int, double)
 					File.read((char*)&box, sizeof(box));
 					Katalog.push_back(make_shared<Motocykl>(mar, mod, da, poj, pow, box));
-					//string mar, string mod, int dat, double poj, double pow, bool b
-					//Katalog.push_back(make_shared<Motocykl>(a, b, c, d, e, f));
+
 				}
 				else if (typ == 'O') {
-					int pas;
+					int pas;//zaleznie od typu -> rozne atrybuty (bool, int, double)
 					File.read((char*)&pas, sizeof(pas));
 					Katalog.push_back(make_shared<Osobowy>(mar, mod, da, poj, pow, pas));
-					//string mar, string mod, int dat, double poj, double pow, bool b
-					//Katalog.push_back(make_shared<Motocykl>(a, b, c, d, e, f));
+
 				}
 				else if (typ == 'L') {
-					double lad;
+					double lad;//zaleznie od typu -> rozne atrybuty (bool, int, double)
 					File.read((char*)&lad, sizeof(lad));
 					Katalog.push_back(make_shared<LKW>(mar, mod, da, poj, pow, lad));
-					//string mar, string mod, int dat, double poj, double pow, bool b
-					//Katalog.push_back(make_shared<Motocykl>(a, b, c, d, e, f));
+
 				}
-				Katalog.back()->setTyp(typ);
-				Katalog.back()->setId(numer);
+				Katalog.back()->setTyp(typ); 
+				Katalog.back()->setId(numer); //nadanie nowych id
 				numer += 1;
-
-
-
 			}
-
 		}
-
 	}
 
-	~Data() {}
 
-	//friend void sortby();
+	~Data() {} //destruktor
 
 
 private:
-	//vector < unique_ptr<Pojazd>> Vehicles;
-	//vector <Pojazd> Vehicles;
-	unique_ptr<Pojazd>* Veh;
-	unique_ptr<Pojazd> dum;
-	vector  <shared_ptr<Pojazd>> Katalog;
-	int numer = 1;
+
+	vector  <shared_ptr<Pojazd>> Katalog; //glowny wektor do trzymania pojazdow
+	int numer = 1; //pomocniczy numerator do nadawnia ID
 };
-//
 
-//
-bool operator<(const Pojazd& P1, const Pojazd& P2) {
-	return (P1.power < P2.power);
-	/*cout << "m - moc, p - pojemnosc, d- data, w-wiek" << endl;
-	string input;
-	cin >> input;
-	if (input == "m" || input == "moc") {
 
-	}
-	 input == "p" || input == "pojemnosc") {
-		return (P1.pojemnosc < P2.pojemnosc);
-	}
-	if (input == "d" || input == "data") {
-		return (P1.data < P2.data);
-	}
-	if (input == "w" || input == "wiek") {
-		return (P1.wiek < P2.wiek);
-	}
-	else {
-		return (P1.id < P2.id);
-	}*/
 
-	//	return (P1.power < P2.power);
-}
 
-/*void sortby(vector  <shared_ptr<Pojazd>> Katalog) {
-		sort(Katalog.begin(), Katalog.end(),
-		[](const Pojazd& a, const Pojazd& b) -> bool
-		{
-			return a.data < b.data;
-		});
-}*/
+
+
+
+
 
 
 class Konsola {
 public:
-	//friend Firma;
 	Data A;
 
 	Konsola() {}
 
 	void akcja() {
-		int stan = 1;
+		//int stan = 1;
 		string akcje;
 		cout << "Welcome to Bjarne Inc." << endl;
 
-		for (int turn = 1; turn <= 50; turn++) {
+		for (int turn = 1; turn <= 100; turn++) {//100 ruchow, dowolna liczba
 
-			cout << "Choose wisely: lp,m,rm,break,cech,sortby" << endl;
+			cout << "Choose wisely: lp (listuj pojazdy) ,m (moto) ,rm (random moto), l (lorry), rl (random lorry) o (osobowy), ro (randmo osobowy),time (wiek+1),more (liczba wpisow o x>limit),sortby (sortuj po), er (wymaz pojazd), write (zapisz do pliku .bin), read(wczytaj ze pliku .bin),break (koniec)" << endl;
 			cout << "Ruch numer: " << turn << endl << endl;
 			cin >> akcje;
-			if (akcje == "lp") {
+			if (akcje == "lp") {//listuj pojazdy
 				A.drukuj();
 				cout << endl;
 			}
-			if (akcje == "m") {
+			if (akcje == "m") {//wstaw motocykl (random/custom)
 				A.dodaj('M');
 				cout << endl;
 			}
-			if (akcje == "rm") {
+			if (akcje == "rm") {//wstaw random motocykl
 				A.dodaj_random('M');
 				cout << endl;
 			}
-			if (akcje == "o") {
+			if (akcje == "o") {//wstaw osobowy (random/custom)
 				A.dodaj('O');
 				cout << endl;
 			}
-			if (akcje == "ro") {
+			if (akcje == "ro") { //wstaw random osobowy
 				A.dodaj_random('O');
 				cout << endl;
 			}
-			if (akcje == "l") {
+			if (akcje == "l") { //wstaw lorry (custom/random)
 				A.dodaj('L');
 				cout << endl;
 			}
-			if (akcje == "rl") {
+			if (akcje == "rl") { //wstaw random lorry
 				A.dodaj_random('L');
 				cout << endl;
 			}
-			if (akcje == "time") {
+			if (akcje == "time") {//mija rok, wiek rosnie o 1
 				A.timeflow();
 				cout << endl;
 			}
@@ -526,16 +439,13 @@ public:
 				A.drukuj();
 				cout << endl;
 			}
-			if (akcje == "sortby") {
+			if (akcje == "sortby") { //sortowanie po zmiennej + drukowanie
 				A.sortby();
 				A.drukuj();
 				cout << endl;
 			}
-			if (akcje == "cech") {
-				A.dodaj_cechy();
-				cout << endl;
-			}
-			if (akcje == "er") {
+
+			if (akcje == "er") { //usuwanie wiersza
 				cout << "Gimme a id that you would like to erase\t";
 				int nr;
 				cin >> nr;
@@ -547,17 +457,16 @@ public:
 				string aux;
 				cin >> aux;
 				//A.maxi(Pojazd::getPower());
-
 				cout << endl;
 			}
 
-			if (akcje == "more") {
+			if (akcje == "more") {//sprawdz, ile pojazdow ma wiekszy rok/moc
 				cout << "Which variable? Data (d)or Power(p)?\t" << endl;
 				char type;
 				cin >> type;
 				if (type == 'p') {
 					cout << endl << "Power bigger than...? Gimme double\t";
-					double limit;
+					double limit; //limit to nasz podzial, chcemy dostac wiecej niz limit
 					cin >> limit;
 					cout << " There are more than " << A.more(limit) << " vehciles that meet the criteria" << endl; ;
 				}
@@ -569,17 +478,17 @@ public:
 				}
 			}
 
-			if (akcje == "write") {
+			if (akcje == "write") { //zapiasz do pliku bin.
 				A.Write();
 				cout << "write" << endl;
 
 			}
-			if (akcje == "read") {
+			if (akcje == "read") { //odczytaj z pliku bin
 				A.Read();
 				cout << "read" << endl;
 
 			}
-			if (akcje == "break") {
+			if (akcje == "break") { //zakoncz
 
 				cout << "SEEYA" << endl;
 				break;
@@ -596,10 +505,7 @@ private:
 
 int main()
 {
-	ofstream myfile;
-	myfile.open("dane.txt");
-	myfile << "Boo.\n";
-	myfile.close();
+
 
 	//Motocykl A();
 	//Data D;
--- pojazdy.hpp
+++ pojazdy.hpp
@@ -3,7 +3,6 @@
 #include "baza_new.h"
 #include "randomgen.h"
 #include <vector>
-#include <list>//
 #include <string>
 #include <iostream>
 #include <fstream>
@@ -14,7 +13,7 @@
 
 using namespace std;
 
-
+/*
 template<typename D, typename B>
 unique_ptr<D> static_cast_ptr(unique_ptr<B>& base) {
 	return unique_ptr<D>(static_cast<D*>(base.release()));
@@ -22,24 +21,25 @@ unique_ptr<D> static_cast_ptr(unique_ptr<B>& base) {
 template<typename D, typename B>
 unique_ptr<D> static_cast_ptr(unique_ptr<B>&& base) {
 	return unique_ptr<D>(static_cast<D*>(base.release()));
-}
+}*/
 
 class Pojazd {
 public:
-	Pojazd() : marka(Random_MarkaG()), model(Random_ModelG()), data(Random_Data()), pojemnosc(Random_Poj()), power(Random_Power()) { }
-	Pojazd(string mar, string mod, int dat, double poj, double pow) :marka(mar), model(mod), data(dat), pojemnosc(poj), power(pow) {}
-	int getId() { return id; }//const const
+	Pojazd() : marka(Random_MarkaG()), model(Random_ModelG()), data(Random_Data()), pojemnosc(Random_Poj()), power(Random_Power()) { } //konstruktor domyslny, wykorzystuje randomizowane param
+	Pojazd(string mar, string mod, int dat, double poj, double pow) :marka(mar), model(mod), data(dat), pojemnosc(poj), power(pow) {} //konstruktor param, przyjmuje nasze dane/ dane z pliku
+	const int getId() const { return id; }//gettery i settery...
 	const int getWiek() const { return wiek; }
 	const double getData() const { return data; }//int
-	const double getPower() const { return power; }//int
-	const double getPojemnosc() const { return pojemnosc; }//int nowosc
-	list<string> getCechy() const { return cechy; }////
-	const string getModel() const { return model; }//int
-	const string getMarka() const { return marka; }//int
+	const double getPower() const { return power; }//double
+	const double getPojemnosc() const { return pojemnosc; }//double
+	//list<string> getCechy() const { return cechy; }////
+	const string getModel() const { return model; }//string
+	const string getMarka() const { return marka; }//string
 	const char getTyp()  const { return typ; }
 	void setId(int ID) { id = ID; }
 	void setTyp(char t) { typ = t; }
-	void setCechy() {
+
+	/*void setCechy() {
 
 		cout << "Max 3 cechy!" << endl;
 		for (int i = 0; i < 3; ++i) {
@@ -60,20 +60,15 @@ public:
 		}
 
 	}
-	//Pojazd& operator++(int) {
-	//	++wiek;
-	//	return *this;
-	//	}
-	friend bool operator<(const Pojazd& P1, const Pojazd& P2);
-	//friend bool fun(const Pojazd& P1, const Pojazd& P2);
-
-	//int data;
+	*/
+	
+	//friend bool operator<(const Pojazd& P1, const Pojazd& P2);//sama deklaracja
 
 	void print() { cout << "Marka:" << marka << ", model:" << model << ", data:" << data << ", wiek:" << wiek << ", poj:" << pojemnosc << ", moc:" << power << endl; }
 	virtual ~Pojazd() { cout << "Bye Pojazd" << endl; }
 
 
-	friend Pojazd& operator++(Pojazd& L);
+	friend Pojazd& operator++(Pojazd& L); //sama deklaracja
 private:
 	string marka;
 	string model;
@@ -82,58 +77,58 @@ private:
 	double pojemnosc;
 	int liczba_cech;
 	int wiek = 2022 - data;
-	bool diesel;
 	char typ;
 	int id;
-	list<string> cechy;
 
-
-
-
-	//friend void sortby(vector  <shared_ptr<Pojazd>> Katalog);
-	//friend void sortby();
 };
 
-Pojazd& operator++(Pojazd& L) {
+Pojazd& operator++(Pojazd& L) {//oeprator ++ dla pojazdow, wykorzystamy go do funkcji void timeline
 	++L.wiek;
 	return L;
 }
 
+/*
+bool operator<(const Pojazd& P1, const Pojazd& P2) {
+	return (P1.power < P2.power);
+}*/
 
 
 class Motocykl :public Pojazd {
 public:
 	//const char typ = 'M';//static
-	Motocykl() :Pojazd((Random_MarkaM()), (Random_ModelM()), (Random_Data()), (Random_Poj()), (Random_Power())), boxer(Random_Boxer()) {}
-	Motocykl(string mar, string mod, int dat, double poj, double pow, bool b) :Pojazd(mar, mod, dat, poj, pow), boxer(b) {}
+	Motocykl() :Pojazd((Random_MarkaM()), (Random_ModelM()), (Random_Data()), (Random_Poj()), (Random_Power())), boxer(Random_Boxer()) {} //domyslny -randomizacja
+	Motocykl(string mar, string mod, int dat, double poj, double pow, bool b) :Pojazd(mar, mod, dat, poj, pow), boxer(b) {} //param
 	virtual void print() { cout << "\tBoxer?:\t" << boxer << endl; }
 	virtual ~Motocykl() { cout << "Bye motocykl" << endl; }
-	bool getBool() { return boxer; }
+	const bool getBool() const{ return boxer; }
 private:
-	bool boxer;
+	bool boxer;//unikalny atrybut motocyklu
 };
+
 
 class Osobowy :public Pojazd {
 public:
 //	const char typ = 'O';//static
-	Osobowy() :Pojazd((Random_MarkaO()), (Random_ModelO()), (Random_Data()), (Random_Poj()), (Random_Power())), pasazerowie(Random_Pasazerowie()) {}
-	Osobowy(string mar, string mod, int dat, double poj, double pow, int p) :Pojazd(mar, mod, dat, poj, pow), pasazerowie(p) {}
+	Osobowy() :Pojazd((Random_MarkaO()), (Random_ModelO()), (Random_Data()), (Random_Poj()), (Random_Power())), pasazerowie(Random_Pasazerowie()) {}//domyslny -randomizacja
+	Osobowy(string mar, string mod, int dat, double poj, double pow, int p) :Pojazd(mar, mod, dat, poj, pow), pasazerowie(p) {} //param
 	virtual void print() { cout << "\tPasazerowie?:\t" << pasazerowie << endl; }
 	virtual~Osobowy() { cout << "Bye osobowy" << endl; }
-	int getPasazerowie() { return pasazerowie; }
+	const int getPasazerowie() const { return pasazerowie; }
 private:
-	int pasazerowie;
+	int pasazerowie; //unikalny dla osobowy
 
 };
+
+
 class LKW :public Pojazd {
 public:
 	//const char typ = 'P';//static
-	LKW() :Pojazd((Random_MarkaL()), (Random_ModelL()), (Random_Data()), (Random_Poj()), (Random_Power())), ladunek(Random_Ladunek()) {}
-	LKW(string mar, string mod, int dat, double poj, double pow, double l) :Pojazd(mar, mod, dat, poj, pow), ladunek(l) {}
+	LKW() :Pojazd((Random_MarkaL()), (Random_ModelL()), (Random_Data()), (Random_Poj()), (Random_Power())), ladunek(Random_Ladunek()) {}//default - randomizacja
+	LKW(string mar, string mod, int dat, double poj, double pow, double l) :Pojazd(mar, mod, dat, poj, pow), ladunek(l) {}//param
 	virtual void  print() { cout << "\tLadunek?:\t" << ladunek << endl; }
 	virtual ~LKW() { cout << "Bye lkw" << endl; }
-	double getLadunek() { return ladunek; }
+	const double getLadunek() const { return ladunek; }
 private:
-	double ladunek;
+	double ladunek;//unikalny dla lkw
 
 };
